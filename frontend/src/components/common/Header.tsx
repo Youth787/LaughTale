fd0914f7027a1196eb08120cfdf56aa399214e13
yeonmi <ar77gt@daum.net>
2024-03-19 16:05:21 +0900

    design :mypage
    Former-commit-id: 2a6898cbf21fc5a7056ecd5a8dd79eb2e811e4f4

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,9 +1,3 @@
-import Logo from '../../assets/logo.png';
-
-const Header = () => {
-    return <div className="bg-[rgba(83,82,95,0.23)] flex justify-center items-center">
-        <img src={Logo} height="200" width="170"></img>
-    </div>
-}
-
-export default Header;
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2f578e522d37549629aa88de4e2a969748deae6d6dc8268a0de8a27f1b975c6
+size 245
