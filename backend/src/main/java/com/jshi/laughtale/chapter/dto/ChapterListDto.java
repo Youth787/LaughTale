31e330b9b8396cf00c937debb6c6e7e3412dc946
jinooyang <jinooyang@gmail.com>
2024-03-25 11:00:20 +0900

    fix :api/chapter/list : added thumbnail image
    Former-commit-id: b79c08265ab1a3bbf216fe12823f06fc40455470

--- backend/src/main/java/com/jshi/laughtale/chapter/dto/ChapterListDto.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/dto/ChapterListDto.java
@@ -1,23 +1,3 @@
-package com.jshi.laughtale.chapter.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-public class ChapterListDto {
-
-
-
-	@Getter
-	@Builder
-	public static class Request {
-
-	}
-
-	@Getter
-	@Builder
-	public static class Response {
-		private Long chapterId;
-		private Integer chapterNo;
-
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:372ecd1c3d032fbbdd3f519803c5ecb10b27d79a1ae17e089378f8eb21da3890
+size 314
