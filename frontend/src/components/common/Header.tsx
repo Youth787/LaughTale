0bb6921c1bb2f810250548f2fcb53fad78d94dde
Jaechan Kim <penguin_03@naver.com>
2024-03-27 16:33:04 +0900

    fix: builderror
    Former-commit-id: e02fdc38ae672cf82dec794f9fd6b8b42aa3439a

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdf24e6132c06d9f4d486e05c60513269185d333b053ede8d6a755dcace7a7a9
-size 244
+oid sha256:f3e591b3a28bea4fc6f7100c7765cc8bf703386467ab9005986aff976909d6d9
+size 640
