3d4b36f4ae0353507c841f11a7360df957faa7be
Jaechan Kim <penguin_03@naver.com>
2024-03-27 16:33:04 +0900

    fix: builderror
    Former-commit-id: e02fdc38ae672cf82dec794f9fd6b8b42aa3439a

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40ba69eddb4672cdc14557106d3d58baa691bdcb2c166d97ef85bd0b735381c0
-size 182
+oid sha256:2c0aa316d3c294f3505d8a0aee5468b492a7543510d2139a2e31ec37020993b2
+size 216
