115ad65088a121183aae95b9bbe1e476f31f486f
jaechan <penguin_03@naver.com>
2024-03-27 20:52:33 +0900

    feat: 타입추가
    Former-commit-id: ae2d4392717dd5047aa32b6e4bd53e5bedbe8016

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68d112e89d9048b6b3ff5c1cf85efb0404a4e45e9ab57515105ef7087dc2828e
-size 226
+oid sha256:ef0b30ac10790eaeb234e2d42f039dcdb6cd05ab0d8a23d8e0a7e75c36c4307e
+size 247
