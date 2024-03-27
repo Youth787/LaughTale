6a6eb2ba7c1b9963def8f1fc4a22d0181b6f79f1
Jaechan Kim <penguin_03@naver.com>
2024-03-27 10:12:41 +0900

    feat: auth button
    Former-commit-id: b1ec2e7a32ca1ec6bd47d9d6aab4a248ef3bc490

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8fef9d303c506d33a85f1ffabd6a7d3631562b407d6a1e1c08495bd70f3c2e57
-size 171
+import axios from "axios";
+import client from "./index.ts";
+
+const getMyInfo = async () => {
+  const {data} = await client.get("/me");
+  return data;
+}
+export {getMyInfo};
