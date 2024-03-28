fcfe22d35aa56798888249502257822b31691121
Jaechan Kim <penguin_03@naver.com>
2024-03-28 11:38:23 +0900

    feat: login 끝
    Former-commit-id: 7ad8c6509d5affdc7225b9ed932e168c27e8f316

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,8 +1,3 @@
-import axios from "axios";
-import client from "./index.ts";
-
-const getMyInfo = async () => {
-  const {data} = await client.get("/me");
-  return data;
-}
-export {getMyInfo};
+version https://git-lfs.github.com/spec/v1
+oid sha256:d27e22fa2744354050d92dd388ade2b8c713469d04826d001c7fa832ce168b80
+size 252
