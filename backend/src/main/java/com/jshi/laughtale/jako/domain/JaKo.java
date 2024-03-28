13a8734fc62d17339c73f19e1aff6b97e67daeb9
jinooyang <jinooyang@gmail.com>
2024-03-28 14:34:48 +0900

    feat : parsed definition (jako and word_data)
    Former-commit-id: f54600364d8a82eb0afda5a715d19c83e201dd99

--- backend/src/main/java/com/jshi/laughtale/jako/domain/JaKo.java
+++ backend/src/main/java/com/jshi/laughtale/jako/domain/JaKo.java
@@ -1,21 +1,3 @@
-package com.jshi.laughtale.jako.domain;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@Entity
-@Table(name = "jako")
-@Getter
-public class JaKo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column
-    private String langFrom;
-    @Column
-    private String langTo;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:72b0ea9f876c35e5ea6a3c2144ce02c314afbfab3878e48ab2832d3ba9a78e1d
+size 475
