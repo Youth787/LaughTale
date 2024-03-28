9b4a0b44549b090785dfa357921a1a85ed7cb09b
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-28 17:17:38 +0900

    feat: 단어장에 단어 추가하기 및 말풍선에 있는 단어 목록 조회 기능
    Former-commit-id: e92ef10d56f8e97cd00e7d8430b7c4dd9df354bb

--- backend/src/main/java/com/jshi/laughtale/quiz/dto/QuizResult.java
+++ backend/src/main/java/com/jshi/laughtale/quiz/dto/QuizResult.java
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:deced2c75e01eb311cc931997a282bcdf2f3371f8d77a34a4144a2d6d2999720
-size 299
+package com.jshi.laughtale.quiz.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class QuizResult {
+	@Getter
+
+	public static class Request {
+		private List<Integer> answer;
+	}
+
+	@Getter
+	@Builder
+	public static class Response {
+		private List<Boolean> result;
+	}
+}
