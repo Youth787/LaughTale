f2d7aef83a230c5e4b60a288f728fdf0aa403c21
dohyeong <ehgud619@naver.com>
2024-03-29 14:20:51 +0900

    fix : 레벨별 만화 목록 띄우기 수정
    Former-commit-id: 6267700439f766120c8cd491eabc498af454235a

--- frontend/src/pages/cartoon/manga/TotalEpisode.tsx
+++ frontend/src/pages/cartoon/manga/TotalEpisode.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c4a5db529fb6e46ca4bb193048da05857239c653ca561ed653ad059cd0c1a448
-size 238
+
+
+
+type Props = {
+  total: number
+}
+export default function TotalEpisode(props: Props) {
+  const {total} = props;
+  return <div className="mt-3 mb-3">
+    <span className="text-white font-semibold text-xl">총 {total}화</span>
+  </div>
+}
