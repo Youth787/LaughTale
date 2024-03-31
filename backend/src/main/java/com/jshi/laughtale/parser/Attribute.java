6d6e9624d060bb7b288a4d9a0eca4629e2d14df7
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-01 00:17:12 +0900

    refactor: 만화 업로드 save와 analyze 기능 분리
    Former-commit-id: 8176fa427d5dfe663ef009915f4b34cc9b2c3d9d

---
+++ backend/src/main/java/com/jshi/laughtale/parser/Attribute.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfc89eb0d8ab421f8b0f1eed55984ad1dd8d64589bbcea8e0956ec41a2fcdaa6
+size 252
---
+++ backend/src/main/java/com/jshi/laughtale/parser/context/MangaContext.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a8181bf0ea1eb3b566c3cc108f4343d7b7746af520fec2111446a911c57ac08
+size 371
