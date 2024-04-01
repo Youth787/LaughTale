db8c045c25fe512e9c50a911b39b1945c94f7a1f
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-01 10:42:01 +0900

    refactor: 만화 분석과 저장 로직 분리
    Former-commit-id: 21fcee52144fbd25bc7336e7364d4803af702b64

--- backend/src/main/java/com/jshi/laughtale/parser/context/MangaContext.java
+++ backend/src/main/java/com/jshi/laughtale/parser/context/MangaContext.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a8181bf0ea1eb3b566c3cc108f4343d7b7746af520fec2111446a911c57ac08
-size 371
+oid sha256:265c6a8f02ebf37b05c829f624124843972028a0f25d59550fb9c505dff044f7
+size 367
