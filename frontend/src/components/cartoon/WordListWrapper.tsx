39e3f7e1fd060a0560acd494f7ef14a56df9e9bb
jaechan <penguin_03@naver.com>
2024-04-02 03:14:05 +0900

    feat: viewer
    Former-commit-id: 312a0c6205a3c670a53708e1f19c4f69e5d2c7f2

--- frontend/src/components/cartoon/WordListWrapper.tsx
+++ frontend/src/components/cartoon/WordListWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3065a795558809cd14a17034a95deb3f6397fd501643c4660c3ae3b48f135040
-size 369
+oid sha256:329a02d37391b1d3fb48576fb8a54ead7e96d50c5e7267fdde24d524afc6369f
+size 543
