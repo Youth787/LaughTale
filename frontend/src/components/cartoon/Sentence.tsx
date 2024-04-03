ca7b0fe872335031f0edb0c8400a23eb5397c69f
Jaechan Kim <penguin_03@naver.com>
2024-04-03 21:34:53 +0900

    feat add close button
    Former-commit-id: d7aa3c42433bec6c8b126eda276dcbf57366d254

--- frontend/src/components/cartoon/Sentence.tsx
+++ frontend/src/components/cartoon/Sentence.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:971f63b158ec46f5bedaa5ae57ebca5bf2c56cbcbd3395f8b47f7ae435b9a6f3
-size 476
+oid sha256:9afbcb51b80c9c5ec357940818b3af7995e70f115caa2ed71d85dfa610c1cc11
+size 613
