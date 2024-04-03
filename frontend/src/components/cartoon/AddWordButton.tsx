845a10ecdb821ed55778e19339fcd06a70869a44
Jaechan Kim <penguin_03@naver.com>
2024-04-03 20:30:00 +0900

    feat: add toast
    Former-commit-id: 55305ac73cc74588ed90aa5d02785e5c4f0671ee

--- frontend/src/components/cartoon/AddWordButton.tsx
+++ frontend/src/components/cartoon/AddWordButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0f5958a3b64dc4eaea13caf83db6088d3d45a0aba866cc75df1217de521db54
-size 474
+oid sha256:ca114e076e85468a05dc005507671553003bf748ddd0ba4e99389a706c059bcd
+size 679
